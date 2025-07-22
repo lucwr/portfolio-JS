0bed98dd7a7460e8c5381c3534762e92afe776eb
bluestar <bluestar@gmail.com>
2025-07-22 10:01:06 -0400

    changed resume url at 07221001

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const clipPaths = [
 const Home: NextPage<Props> = () => {
   const profileData: ProfileData = {
     id: "1",
-    cv: "https://drive.google.com/file/d/1Vy-Q_Li9uQm9bkZMd5860egeFnrBmul-/view?usp=drive_link",
+    cv: "https://docs.google.com/document/d/1fXolHj25B1NF8qDYzwNOvzFuj32C3uaG/edit?usp=drive_link&ouid=106441845075925706201&rtpof=true&sd=true",
     name: "Daniel Lee",
     bgImages: [
       { url: "/images/about/bg_1.jpg" },
